ac4e537bafcb8236cdc34dc2f604d872fbaf30f1
Terinor <kirstruk01@gmail.com>
2023-09-04 17:58:37 +0300

    changed from re

--- lections/module6/sort.py
+++ lections/module6/sort.py
@@ -3,24 +3,6 @@ import shutil
 import re
 import sys
 
-#FOLDER_NAMES = ['archives', 'video', 'audio', 'documents', 'images', '3Dmodels', 'apps', 'torrents']
-
-def normalize(s):
-    translit_dict = {
-        ord('а'): 'a', ord('б'): 'b', ord('в'): 'v', ord('г'): 'h', ord('д'): 'd', 
-        ord('е'): 'e', ord('є'): 'ie', ord('ж'): 'zh', ord('з'): 'z', ord('и'): 'y', 
-        ord('і'): 'i', ord('ї'): 'i', ord('й'): 'i', ord('к'): 'k', ord('л'): 'l', 
-        ord('м'): 'm', ord('н'): 'n', ord('о'): 'o', ord('п'): 'p', ord('р'): 'r', 
-        ord('с'): 's', ord('т'): 't', ord('у'): 'u', ord('ф'): 'f', ord('х'): 'kh', 
-        ord('ц'): 'ts', ord('ч'): 'ch', ord('ш'): 'sh', ord('щ'): 'shch', ord('ь'): '', 
-        ord('ю'): 'iu', ord('я'): 'ia', ord('ы'): 'y', ord('ъ'): '', ord('ё'): 'e'
-    }
-
-    normalized = s.translate(translit_dict)
-    normalized = re.sub(r'[^A-Za-z0-9.]', '_', normalized)
-    return normalized
-
-        
 def create_list_of_files(file_path):
     output_file_path = os.path.join(file_path, "file_list.txt")
     items_list = os.walk(file_path)
@@ -30,9 +12,21 @@ def create_list_of_files(file_path):
             file.write("Adres:" + str(items[0]) + "\n")
             file.write("Folders:" + str(items[1]) + "\n")
             file.write("Files:" + str(items[2]) + "\n" + "\n")
-    
+ 
+def rename_file_with_folder_name(file_path):
+
+    folder_path = os.path.dirname(file_path)
+    folder_name = os.path.basename(folder_path)
+    file_name = os.path.basename(file_path)
+
+    new_name = f"{folder_name}_{file_name}"
+    new_path = os.path.join(folder_path, new_name)
+
+    os.rename(file_path, new_path)
+    return new_path, new_name
 
 def remove_empty_folders(folder_path):
+
     if not os.path.exists(folder_path):
         #print(f"Папка {folder_path} не існує.")
         return
@@ -50,61 +44,97 @@ def remove_empty_folders(folder_path):
             if os.path.isdir(item_path):
                 remove_empty_folders(item_path)
 
+def normalize(s):
+    translit_dict = {
+        ord('а'): 'a', ord('б'): 'b', ord('в'): 'v', ord('г'): 'h', ord('д'): 'd', 
+        ord('е'): 'e', ord('є'): 'ie', ord('ж'): 'zh', ord('з'): 'z', ord('и'): 'y', 
+        ord('і'): 'i', ord('ї'): 'i', ord('й'): 'i', ord('к'): 'k', ord('л'): 'l', 
+        ord('м'): 'm', ord('н'): 'n', ord('о'): 'o', ord('п'): 'p', ord('р'): 'r', 
+        ord('с'): 's', ord('т'): 't', ord('у'): 'u', ord('ф'): 'f', ord('х'): 'kh', 
+        ord('ц'): 'ts', ord('ч'): 'ch', ord('ш'): 'sh', ord('щ'): 'shch', ord('ь'): '', 
+        ord('ю'): 'iu', ord('я'): 'ia', ord('ы'): 'y', ord('ъ'): '', ord('ё'): 'e',
+        ord('А'): 'A', ord('Б'): 'B', ord('В'): 'V', ord('Г'): 'H', ord('Д'): 'D', 
+        ord('Е'): 'E', ord('Є'): 'Ye', ord('Ж'): 'Zh', ord('З'): 'Z', ord('И'): 'Y', 
+        ord('І'): 'I', ord('Ї'): 'Yi', ord('Й'): 'Y', ord('К'): 'K', ord('Л'): 'L', 
+        ord('М'): 'M', ord('Н'): 'N', ord('О'): 'O', ord('П'): 'P', ord('Р'): 'R', 
+        ord('С'): 'S', ord('Т'): 'T', ord('У'): 'U', ord('Ф'): 'F', ord('Х'): 'Kh', 
+        ord('Ц'): 'Ts', ord('Ч'): 'Ch', ord('Ш'): 'Sh', ord('Щ'): 'Shch', ord('Ь'): '', 
+        ord('Ю'): 'Yu', ord('Я'): 'Ya', ord('Ы'): 'Y', ord('Ъ'): '', ord('Ё'): 'E'
+    }
+
+    normalized = s.translate(translit_dict)
+    normalized = re.sub(r'[^A-Za-z0-9.]', '_', normalized)
+    return normalized
+        
+def normalize_objects_in_directory(directory_path):
+    for root, dirs, files in os.walk(directory_path, topdown=False):
+        for folder_name in dirs:
+            folder_path = os.path.join(root, folder_name)
+            new_name = normalize(folder_name)
+            new_path = os.path.join(root, new_name)
+            
+            if new_name != folder_name:
+                os.rename(folder_path, new_path)
+        
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            new_name = normalize(file_name)
+            new_path = os.path.join(root, new_name)
+            
+            if new_name != file_name:
+                os.rename(file_path, new_path)
+
 def sort_directory(directory_path, categories):
-    
-    
-    for item in os.listdir(directory_path):
-        item_path = os.path.join(directory_path, item)
-        
-        if item == "file_list.txt":
-            continue
 
-        new_name = normalize(item)
-        
-        if new_name != item:
-            new_path = os.path.join(directory_path, new_name)
-            os.rename(item_path, new_path)
-            item_path = new_path
-
-        if os.path.isdir(item_path):
-            if item not in categories.keys():
-                sort_directory(item_path, categories)
-            
-        else:
-
-
-            
-            unknown_extension = True
-            file_extension = os.path.splitext(item_path)[1].lower()
-            for category, extensions in categories.items():
-
-                if file_extension in extensions:
-                    
+    normalize_objects_in_directory(directory_path)
+      
+    for root, folders, files in os.walk(directory_path):
+        if os.path.basename(root) not in categories.keys():   
+            for file_name in files:
+                file_path = os.path.join(root, file_name)
+                                                
+                unknown_extension = True
+                file_extension = os.path.splitext(file_name)[1].lower()
+                for category, extensions in categories.items():
+                    if file_extension in extensions:
                         dest_folder = category
                         dest_folder_path = os.path.join(directory_path, dest_folder)
-                        
                         unknown_extension = False
-                        
                         os.makedirs(dest_folder_path, exist_ok=True)
-                        
 
                         if file_extension in ('.zip', '.gz', '.tar'):
-                            archive_dest_folder_path = os.path.join(dest_folder_path, os.path.splitext(item)[0])
+                            archive_dest_folder_path = os.path.join(dest_folder_path, os.path.splitext(file_name)[0])
                             os.makedirs(archive_dest_folder_path, exist_ok=True)
-                            shutil.unpack_archive(item_path, archive_dest_folder_path)
-                            os.remove(item_path)
+                            shutil.unpack_archive(file_path, archive_dest_folder_path)
+                            os.remove(file_path)
                         else:
-                            shutil.move(item_path, dest_folder_path)
-                        break
+                            file_path, file_name = rename_file_with_folder_name(file_path)
+                            dest_file_path = os.path.join(dest_folder_path, file_name)
+                            # Перевіряємо, чи файл вже існує в папці призначення
+                            if os.path.isfile(dest_file_path):
+                                # Отримуємо інформацію про файл, який вже існує
+                                existing_file_stat = os.stat(dest_file_path)
+                                
+                                # Отримуємо інформацію про поточний файл, який ми намагаємося перемістити
+                                current_file_stat = os.stat(file_path)
+                                
+                                # Перевіряємо, чи поточний файл є новішим за існуючий за датою створення
+                                if current_file_stat.st_ctime > existing_file_stat.st_ctime:
+                                    # Якщо поточний файл новіший, то перейменовуємо та переміщаємо його
+                                    shutil.move(file_path, dest_file_path)
+                                    
+                                # В іншому випадку, якщо існуючий файл новіший, не робимо нічого
+                            else:
+                                # Якщо файлу з такою назвою не існує, просто переміщуємо його
+                                shutil.move(file_path, dest_file_path)
 
-                        
-            if unknown_extension:
-                print(f"Unknown file extension: {item_path}")
+                        break
+                    
+                if unknown_extension:
+                    print(f"Unknown file extension: {file_path}")
+
     remove_empty_folders(directory_path)
-    try:
-        create_list_of_files(directory_path)
-    except FileNotFoundError:
-        print("Folder was empty and deleted")
+    create_list_of_files(directory_path)
     
 
 if __name__ == "__main__":
@@ -120,7 +150,7 @@ if __name__ == "__main__":
                 'images': ('.jpg', '.png', '.jpeg', '.svg'),
                 'video': ('.avi', '.mp4', '.mov', '.mkv'),
                 'audio': ('.mp3', '.ogg', '.wav', '.amr'),
-                'documents': ('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'),
+                'documents': ('.doc', '.docx', '.docm', '.txt', '.pdf', '.xlsx', '.xls', '.pptx'),
                 'archives': ('.zip', '.gz', '.tar', '.rar', '.7z', '.iso'),
                 '3Dmodels': ('.stl'),
                 'apps': ('.exe', '.bin', '.msi'),
